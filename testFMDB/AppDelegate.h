17361eb92125c0b3a4825e0b31ca0fdef767b497
yl <yanli@yellfun.com>
2016-08-22 17:31:37 +0800

    test

---
+++ testFMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFMDB
+//
+//  Created by ky on 8/22/16.
+//  Copyright © 2016 yellfun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
